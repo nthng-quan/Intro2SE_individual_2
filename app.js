5554bb55165e8c5998c2924283a7c2cece354298
nthng-quan <hoangquan26102002@gmail.com>
2022-12-09 13:43:21 +0700

    Connect to DB

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express =  require('express');
 const app = express();
-// const port = 3000;
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+mongoose.set('strictQuery', true);
+
+const port = 3000;
+
+// ROUTES
 app.get('/', (req, res) => { 
     res.send('Hello World!');
 });
 
-app.listen(3000);
-// ROUTES
+// connect to DB
+mongoose.connect(
+    process.env.MONGODB_CSTRING, () => { 
+    console.log('connected to DB!');
+});
+
+
+// Listen
+app.listen(port);
