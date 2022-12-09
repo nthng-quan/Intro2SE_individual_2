37c3a30d383c4bafd87da8a2c5d2e98ad358af89
nthng-quan <hoangquan26102002@gmail.com>
2022-12-09 14:27:29 +0700

    Get all/specific post

--- routes/posts.js
+++ routes/posts.js
@@ -2,12 +2,39 @@ const express = require('express');
 const router  = express.Router();
 const Post = require('../models/post');
 
-router.get('/', (req, res) => {
-    res.send('We are on posts');
+// Get back all the posts
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.json(posts);
+    }
+    catch(err) {
+        res.json({ message: err });
+    }
 });
 
-router.post('/', (req, res) => { 
-    console.log(req.body);
+// Submit a post
+router.post('/', async (req, res) => { 
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+    try {
+        const savedPost = await post.save()
+        res.json(savedPost);
+    } catch(err) {
+        res.json({ message: err });
+    }
+});
+
+// Get a specific post
+router.get('/:postId', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    } catch(err) {
+        res.json({ message: err });
+    }
 });
 
 module.exports = router;
